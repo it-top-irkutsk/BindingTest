7be793b74042491b0122bbbdc6bd9d40f5416678
Starinin Andrey <starininandrey@gmail.com>
2022-02-15 16:23:47 +0300

    Init

---
+++ BindigTest.App/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using BindigTest.Lib;
+
+namespace BindigTest.App
+{
+    public partial class MainWindow : Window
+    {
+        public Person Person { get; set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Person = new Person
+            {
+                Id = 1,
+                FirstName = "Andrey",
+                LastName = "Starinin"
+            };
+        }
+    }
+}
